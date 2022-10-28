13e981d426a2b74558d81b4d95dc937f18195811
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-10-28 10:16:16 +0000

    upgrade whole replit

--- replit.nix
+++ replit.nix
@@ -1,13 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python310
-    pkgs.python310Packages.pip
-    pkgs.python310Packages.idna
-    pkgs.python310Packages.flask
-    pkgs.python310Packages.boto3
-    pkgs.python310Packages.poetry
-    pkgs.python310Packages.discordpy
-    pkgs.python310Packages.validators
+    pkgs.python311
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
@@ -19,7 +12,7 @@
       # Needed for matplotlib
       pkgs.xorg.libX11
     ];
-    PYTHONBIN = "${pkgs.python310}/bin/python";
+    PYTHONBIN = "${pkgs.python311}/bin/python";
     LANG = "en_US.UTF-8";
   };
 }
