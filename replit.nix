a4ccc5a42dc32047b19d2e48e41106644c5ee691
sanyer <sanyer@users.noreply.github.com>
2023-09-28 23:08:53 +0000

    add some previous replit stuff

--- replit.nix
+++ replit.nix
@@ -1,11 +1,23 @@
 { pkgs }: {
   deps = [
     pkgs.vim
-    pkgs.poetry
+    pkgs.htop
     pkgs.ruff
+    pkgs.poetry
     pkgs.python311Full.out
     pkgs.python311Packages.poetry-core
   ];
   env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python311}/bin/python";
+    LANG = "en_US.UTF-8";
   };
 }
