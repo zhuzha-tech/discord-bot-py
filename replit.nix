8837e6a74ef1968c29d0d84e48c0d80bcb8ab1bd
Roman Zhuzha <sanyer@users.noreply.github.com>
2023-09-29 00:51:44 +0200

    upgrade things (#1)
    * revert to python3.8
    
    * add ignore __pycache__
    
    * add venv
    
    * put back venv
    
    * add more deps
    
    * upgrade replit to python311
    
    ---------
    
    Co-authored-by: Replit user <>
    Co-authored-by: romazhuzha <8017180-romazhuzha@users.noreply.replit.com>

--- replit.nix
+++ replit.nix
@@ -1,19 +1,11 @@
 { pkgs }: {
   deps = [
-    pkgs.htop
-    pkgs.python311
+    pkgs.vim
+    pkgs.poetry
+    pkgs.ruff
+    pkgs.python311Full.out
+    pkgs.python311Packages.poetry-core
   ];
   env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
-    ];
-    PYTHONBIN = "${pkgs.python311}/bin/python";
-    LANG = "en_US.UTF-8";
   };
 }
